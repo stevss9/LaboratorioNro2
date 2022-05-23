9bc68c6beb0f3d645ed966047ebb5afae5e04fda
Steveens97 <sinchiguanomoreira@gmail.com>
2022-05-23 15:21:32 -0500

    conexiones a sql

--- todoapp.py
+++ todoapp.py
@@ -1,15 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask_mysqldb import MySQL
 
 app=Flask(__name__)
+app.config['MYSQL_HOST']='DESKTOP-11JM9HJ'
+app.config['MYSQL_USER']='sa'
+app.config['MYSQL_PASSWORD']='090700'
+app.config['MYSQL_DB']='Lab2'
+mysql= MySQL(app)
 
 @app.route("/")
-def home():
-    return render_template("index.html")
+def index():
+    return 'HOLA MUNDO'
 
-@app.route("/agregar")
-def agregar():
-    return render_template("agregar.html")
+@app.route("/add")
+def add():
+    return 'Agregar productos a la lista'
 
+@app.route("/edit")
+def edit():
+    return 'Editar productos a la lista'
+
+@app.route("/delete")
+def delete():
+    return 'Eliminar productos a la lista'
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(port = 3000, debug = True)
