a8d29f325c02d20df84890cb92768849704ea477
Steveens97 <sinchiguanomoreira@gmail.com>
2022-05-24 22:26:08 -0500

    agregamos areas

--- todoapp.py
+++ todoapp.py
@@ -1,5 +1,4 @@
-from distutils.log import debug
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -7,11 +6,26 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////listas.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
 db = SQLAlchemy(app)
 
+class Tarea(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text)
+    done = db.Column(db.Boolean, default=False)
+
+    def __init__(self, content):
+        self.content = content
+        self.done = False
+
 db.create_all()
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/tareas', methods=['POST'])
+def agregar_tarea():
+    content = request.form['content']
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
