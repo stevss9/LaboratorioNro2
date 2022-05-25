05bd98101413fec0e5daa8e935f9ea4b36f04349
Steveens97 <sinchiguanomoreira@gmail.com>
2022-05-24 22:03:11 -0500

    nueva conexion a la base de datos

--- todoapp.py
+++ todoapp.py
@@ -1,28 +1,17 @@
+from distutils.log import debug
 from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_mysqldb import MySQL
+from flask_sqlalchemy import SQLAlchemy
 
-app=Flask(__name__)
-app.config['MYSQL_HOST']='DESKTOP-11JM9HJ'
-app.config['MYSQL_USER']='sa'
-app.config['MYSQL_PASSWORD']='090700'
-app.config['MYSQL_DB']='Lab2'
-mysql= MySQL(app)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////listas.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
+db = SQLAlchemy(app)
 
-@app.route("/")
+db.create_all()
+
+@app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route("/add", method=)
-def add():
-    return 'Agregar productos a la lista'
-
-@app.route("/edit")
-def edit():
-    return 'Editar productos a la lista'
-
-@app.route("/delete")
-def delete():
-    return 'Eliminar productos a la lista'
-
-if __name__=="__main__":
-    app.run(port = 3000, debug = True)
+if __name__ == '__main__':
+    app.run(debug=True)
